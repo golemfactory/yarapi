47354ecf30dcd04d671f36f44eed87ec6da1ab2a
nieznany.sprawiciel <witek@golem.network>
2020-12-19 14:16:03 +0100

    Messeging from Requestor to ExeUnit

--- src/rest/streaming/messaging.rs
+++ src/rest/streaming/messaging.rs
@@ -5,9 +5,13 @@ use serde::Serialize;
 use std::fs;
 use std::io::{self, Write};
 use std::path::{Path, PathBuf};
+use std::sync::atomic::AtomicUsize;
+use std::sync::atomic::Ordering;
 use std::sync::Arc;
 use tokio::sync::broadcast;
 use tokio::sync::mpsc;
+
+use crate::rest::Transfers;
 
 pub trait ExeUnitMessage: Serialize + DeserializeOwned + Send + Sync {}
 
@@ -123,4 +127,31 @@ fn spawn_file_notifier(tracked_dir: &Path) -> anyhow::Result<broadcast::Sender<P
     });
 
     Ok(sender)
+}
+
+#[derive(Clone)]
+pub struct MessagingRequestor<ActivityImpl: Transfers> {
+    activity: Arc<ActivityImpl>,
+    msg_counter: Arc<AtomicUsize>,
+    tracked_dir: PathBuf,
+}
+
+impl<ActivityImpl: Transfers> MessagingRequestor<ActivityImpl> {
+    pub fn new(
+        activity: Arc<ActivityImpl>,
+        tracked_dir: &Path,
+    ) -> MessagingRequestor<ActivityImpl> {
+        MessagingRequestor {
+            activity,
+            msg_counter: Arc::new(AtomicUsize::new(0)),
+            tracked_dir: tracked_dir.to_path_buf(),
+        }
+    }
+
+    pub async fn send(self, msg: &impl ExeUnitMessage) -> anyhow::Result<()> {
+        let filename = format!("msg-{}", self.msg_counter.fetch_add(1, Ordering::SeqCst));
+        let path = self.tracked_dir.join(filename);
+
+        Ok(self.activity.send_json(&path, &msg).await?)
+    }
 }
